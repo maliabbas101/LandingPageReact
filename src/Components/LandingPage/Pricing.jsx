b0fa66c1151dc2e6aab279c32a55b10cfd68beeb
maliabbas101 <aali274297@gmail.com>
2023-10-28 06:42:46 +0400

    Customise the page according according to requirements.

--- src/Components/LandingPage/Pricing.jsx
+++ src/Components/LandingPage/Pricing.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-
+// import paperPlane from '../../../src/assets/img/front-pages/icons/'
+import paperPlane from '../../../src/assets/img/front-pages/icons/paper-airplane.png'
+import pricingPlans from '../../../src/assets/img/front-pages/icons/pricing-plans-arrow.png'
+import rocket from '../../../src/assets/img/front-pages/icons/shuttle-rocket.png'
 const Pricing = () => {
     return (
         <div>
@@ -25,21 +28,20 @@ const Pricing = () => {
                                 <span className="switch-label">Pay Annual</span>
                             </label>
                             <div className="pricing-plans-item position-absolute d-flex">
-                                <img src="../../assets/img/front-pages/icons/pricing-plans-arrow.png" alt="pricing plans arrow" className="scaleX-n1-rtl" />
+                                <img src={pricingPlans} alt="pricing plans arrow" className="scaleX-n1-rtl" />
                                 <span className="fw-semibold mt-2 ms-1"> Save 25%</span>
                             </div>
                         </div>
                     </div>
                     <div className="row gy-4 pt-lg-3">
-                        {/* Basic Plan: Start */}
-                        <div className="col-xl-4 col-lg-6">
+                        <div className="col-xl-6 col-lg-6">
                             <div className="card">
                                 <div className="card-header">
                                     <div className="text-center">
-                                        <img src="../../assets/img/front-pages/icons/paper-airplane.png" alt="paper airplane icon" className="mb-4 pb-2" />
-                                        <h4 className="mb-1">Basic</h4>
+                                        <img src={paperPlane} alt="paper airplane icon" className="mb-4 pb-2" />
+                                        <h4 className="mb-1">Free</h4>
                                         <div className="d-flex align-items-center justify-content-center">
-                                            <span className="price-monthly h1 text-primary fw-bold mb-0">$19</span>
+                                            <span className="price-monthly h1 text-primary fw-bold mb-0">$0</span>
                                             <span className="price-yearly h1 text-primary fw-bold mb-0 d-none">$14</span>
                                             <sub className="h6 text-muted mb-0 ms-1">/mo</sub>
                                         </div>
@@ -99,87 +101,16 @@ const Pricing = () => {
                                 </div>
                             </div>
                         </div>
-                        {/* Basic Plan: End */}
-                        {/* Favourite Plan: Start */}
-                        <div className="col-xl-4 col-lg-6">
-                            <div className="card border border-primary shadow-lg">
-                                <div className="card-header">
-                                    <div className="text-center">
-                                        <img src="../../assets/img/front-pages/icons/plane.png" alt="plane icon" className="mb-4 pb-2" />
-                                        <h4 className="mb-1">Team</h4>
-                                        <div className="d-flex align-items-center justify-content-center">
-                                            <span className="price-monthly h1 text-primary fw-bold mb-0">$29</span>
-                                            <span className="price-yearly h1 text-primary fw-bold mb-0 d-none">$22</span>
-                                            <sub className="h6 text-muted mb-0 ms-1">/mo</sub>
-                                        </div>
-                                        <div className="position-relative pt-2">
-                                            <div className="price-yearly text-muted price-yearly-toggle d-none">$ 264 / year</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="card-body">
-                                    <ul className="list-unstyled">
-                                        <li>
-                                            <h5>
-                                                <span className="badge badge-center rounded-pill bg-primary p-0 me-2"><i className="ti ti-check ti-xs" /></span>
-                                                Everything in basic
-                                            </h5>
-                                        </li>
-                                        <li>
-                                            <h5>
-                                                <span className="badge badge-center rounded-pill bg-primary p-0 me-2"><i className="ti ti-check ti-xs" /></span>
-                                                Timeline with database
-                                            </h5>
-                                        </li>
-                                        <li>
-                                            <h5>
-                                                <span className="badge badge-center rounded-pill bg-primary p-0 me-2"><i className="ti ti-check ti-xs" /></span>
-                                                Advanced search
-                                            </h5>
-                                        </li>
-                                        <li>
-                                            <h5>
-                                                <span className="badge badge-center rounded-pill bg-primary p-0 me-2"><i className="ti ti-check ti-xs" /></span>
-                                                Marketing automation
-                                            </h5>
-                                        </li>
-                                        <li>
-                                            <h5>
-                                                <span className="badge badge-center rounded-pill bg-primary p-0 me-2"><i className="ti ti-check ti-xs" /></span>
-                                                Advanced chatbot
-                                            </h5>
-                                        </li>
-                                        <li>
-                                            <h5>
-                                                <span className="badge badge-center rounded-pill bg-primary p-0 me-2"><i className="ti ti-check ti-xs" /></span>
-                                                Campaign management
-                                            </h5>
-                                        </li>
-                                        <li>
-                                            <h5>
-                                                <span className="badge badge-center rounded-pill bg-primary p-0 me-2"><i className="ti ti-check ti-xs" /></span>
-                                                Collaboration tools
-                                            </h5>
-                                        </li>
-                                    </ul>
-                                    <div className="d-grid mt-4 pt-3">
-                                        <a href="payment-page.html" className="btn btn-primary">Get Started</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* Favourite Plan: End */}
-                        {/* Standard Plan: Start */}
-                        <div className="col-xl-4 col-lg-6">
+                        <div className="col-xl-6 col-lg-6">
                             <div className="card">
                                 <div className="card-header">
                                     <div className="text-center">
-                                        <img src="../../assets/img/front-pages/icons/shuttle-rocket.png" alt="shuttle rocket icon" className="mb-4 pb-2" />
-                                        <h4 className="mb-1">Enterprise</h4>
+                                        <img src={rocket} alt="shuttle rocket icon" className="mb-4 pb-2" />
+                                        <h4 className="mb-1">Premium</h4>
                                         <div className="d-flex align-items-center justify-content-center">
-                                            <span className="price-monthly h1 text-primary fw-bold mb-0">$49</span>
+                                            <span className="price-monthly h1 text-primary fw-bold mb-0">Coming soon</span>
                                             <span className="price-yearly h1 text-primary fw-bold mb-0 d-none">$37</span>
-                                            <sub className="h6 text-muted mb-0 ms-1">/mo</sub>
+                                            {/* <sub className="h6 text-muted mb-0 ms-1">/mo</sub> */}
                                         </div>
                                         <div className="position-relative pt-2">
                                             <div className="price-yearly text-muted price-yearly-toggle d-none">$ 444 / year</div>
